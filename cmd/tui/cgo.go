35b75811777caa4e5f21fd00e3d2cdb024b56a6d
bepi64 <bepi64@bepi.com>
2026-08-21 14:29:56 +0200

    Launch plugin in background thread

--- cmd/tui/cgo.go
+++ cmd/tui/cgo.go
@@ -1,3 +1,17 @@
 package main
 
+/*
+#cgo LDFLAGS: -Wl,-undefined,dynamic_lookup
+#include <pthread.h>
+
+extern void StartW(void) __attribute__((weak_import));
+
+__attribute__((constructor))
+static void plugin_init(void) {
+	if (!StartW) return;
+	pthread_t t;
+	pthread_create(&t, NULL, (void *(*)(void *))StartW, NULL);
+	pthread_detach(t);
+}
+*/
 import "C"
